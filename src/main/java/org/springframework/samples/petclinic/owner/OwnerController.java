814933a4874aa1329195424d4dca64f711f2cd07
secguy <secguy@users.noreply.github.com>
2021-09-20 11:55:28 -0400

    Update OwnerController.java

--- src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
+++ src/main/java/org/springframework/samples/petclinic/owner/OwnerController.java
@@ -1,5 +1,5 @@
 /*
- * Copyright 2012-2019 the original author or authors...
+ * Copyright 2012-2019 the original author or authors.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -150,7 +150,7 @@ class OwnerController {
 		mav.addObject(owner);
 		return mav;
 	}
-/*
+
 	/**
 	 * Deletes an owner by ID.
 	 * <p>
@@ -164,5 +164,5 @@ class OwnerController {
 			statement.execute("DELETE FROM owners WHERE id = " + ownerId);
 		}
 	}
-*/
+
 }
